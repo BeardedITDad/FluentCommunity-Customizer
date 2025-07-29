776e11ac1051198b452d19d841b4193d34b39a09
BeardedITDad <84863941+BeardedITDad@users.noreply.github.com>
2025-07-29 13:49:00 -0700

    Delete fluentcommunity-customizer/fluentcommunity-customizer/fluentcommunity-customizer.php

--- fluentcommunity-customizer/fluentcommunity-customizer/fluentcommunity-customizer.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Plugin Name: FluentCommunity Customizer
- * Description: Custom plugin for automating user preferences and space handling in FluentCommunity.
- * Version: 1.0.0
- * Author: Dakota Snow
- * License: GPL2
- */
