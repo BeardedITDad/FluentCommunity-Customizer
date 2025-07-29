b08ee0bbe9398af7cfb391b745925f62e6f7af2a
BeardedITDad <84863941+BeardedITDad@users.noreply.github.com>
2025-07-29 15:48:04 -0700

    Update fluentcommunity-customizer.php

--- fluentcommunity-customizer/fluentcommunity-customizer.php
+++ fluentcommunity-customizer/fluentcommunity-customizer.php
@@ -6,7 +6,13 @@
  * Author: Snow Media Group LLC
  * License: GPL2
  */
+require_once plugin_dir_path(__FILE__) . 'plugin-update-checker/plugin-update-checker.php';
 
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+    'https://github.com/BeardedITDad/Fluentcommunity', // Your repo URL
+    __FILE__,
+    'fluentcommunity-customizer' // Plugin slug
+);
 defined('ABSPATH') || exit;
 
 // Load settings page
